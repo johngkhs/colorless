892080fd00216c8748cd1809fe3ead7f651372ef
John <johngkhs@gmail.com>
2016-09-03 10:47:33 -0500

    Add term dimensions class

--- colorless.py
+++ colorless.py
@@ -8,15 +8,19 @@ import re
 import sys
 import time
 
-class FileIterator:
-    def __init__(self, input_file, screen):
-        self.input_file = input_file
-        self.update_term_dimensions(screen)
+class TerminalDimensions:
+    def __init__(self, screen):
+        self.update(screen)
 
-    def update_term_dimensions(self, screen):
+    def update(self, screen):
         term_dimensions = screen.getmaxyx()
-        self.term_num_rows = term_dimensions[0] - 1
-        self.term_num_cols = term_dimensions[1]
+        self.rows = term_dimensions[0] - 1
+        self.cols = term_dimensions[1]
+
+class FileIterator:
+    def __init__(self, input_file, term_dims):
+        self.input_file = input_file
+        self.term_dims = term_dims
 
     def at_beginning_of_file(self):
         return self.input_file.tell() == 0
@@ -36,16 +40,16 @@ class FileIterator:
                 line = char + line
 
         wrapped_num_chars_in_line = len(line)
-        while wrapped_num_chars_in_line > self.term_num_cols:
-            wrapped_num_chars_in_line -= self.term_num_cols
+        while wrapped_num_chars_in_line > self.term_dims.cols:
+            wrapped_num_chars_in_line -= self.term_dims.cols
         self.input_file.seek(len(line) - wrapped_num_chars_in_line, os.SEEK_CUR)
         return line
 
     def next_line(self):
         line = self.input_file.readline()
-        if len(line) > self.term_num_cols:
-            self.input_file.seek(self.term_num_cols - len(line), os.SEEK_CUR)
-            return line[:self.term_num_cols]
+        if len(line) > self.term_dims.cols:
+            self.input_file.seek(self.term_dims.cols - len(line), os.SEEK_CUR)
+            return line[:self.term_dims.cols]
         return line
 
     def prev_char(self):
@@ -67,10 +71,10 @@ class FileIterator:
     def clamp_forward_seekable_line_count(self, line_count):
         current_position = self.input_file.tell()
         END_OF_FILE = ''
-        for lines_remaining_in_file in range(self.term_num_rows + line_count):
+        for lines_remaining_in_file in range(self.term_dims.rows + line_count):
             if self.next_line() == END_OF_FILE:
                 self.input_file.seek(current_position)
-                return max(0, lines_remaining_in_file - self.term_num_rows)
+                return max(0, lines_remaining_in_file - self.term_dims.rows)
         self.input_file.seek(current_position)
         return line_count
 
@@ -81,7 +85,7 @@ class FileIterator:
 
     def seek_to_one_page_before_end_of_file(self):
         self.seek_end()
-        self.reverse_seek(self.term_num_rows)
+        self.reverse_seek(self.term_dims.rows)
 
 def safe_addstr_row_col(screen, row, col, string):
     try:
@@ -112,12 +116,12 @@ def load_config(config_filepath):
             regex_to_color[re.compile(r'({0})'.format(regex))] = color
     return regex_to_color
 
-def increment_cursor(cursor, count, term_num_cols):
+def increment_cursor(cursor, count, cols):
     while True:
-        if cursor[1] + count < term_num_cols:
+        if cursor[1] + count < cols:
             return (cursor[0], cursor[1] + count)
         else:
-            count -= term_num_cols
+            count -= cols
             cursor = (cursor[0] + 1, cursor[1])
 
 def color_regexes_in_line(screen, line, regex_to_color, prev_cursor, new_cursor, file_iterator):
@@ -129,13 +133,15 @@ def color_regexes_in_line(screen, line, regex_to_color, prev_cursor, new_cursor,
             token_matches_regex = (index % 2 == 1)
             if token_matches_regex:
                 safe_addstr_color(screen, token, curses.color_pair(color))
-            curr_cursor = increment_cursor(curr_cursor, len(token), file_iterator.term_num_cols)
+            curr_cursor = increment_cursor(curr_cursor, len(token), file_iterator.term_dims.cols)
+            if curr_cursor[0] > file_iterator.term_dims.rows:
+                break
     screen.move(*new_cursor)
 
 def redraw_screen(screen, regex_to_color, file_iterator):
     current_position = file_iterator.input_file.tell()
     screen.move(0, 0)
-    while screen.getyx()[0] < file_iterator.term_num_rows:
+    while screen.getyx()[0] < file_iterator.term_dims.rows:
         line = input_file.readline()
         if not line:
             break
@@ -144,32 +150,32 @@ def redraw_screen(screen, regex_to_color, file_iterator):
         new_cursor = screen.getyx()
         color_regexes_in_line(screen, line, regex_to_color, prev_cursor, new_cursor, file_iterator)
     file_iterator.input_file.seek(current_position)
-    screen.move(file_iterator.term_num_rows, 0)
+    screen.move(file_iterator.term_dims.rows, 0)
     screen.clrtoeol()
-    safe_addstr_row_col(screen, file_iterator.term_num_rows, 0, ':')
+    safe_addstr_row_col(screen, file_iterator.term_dims.rows, 0, ':')
     screen.refresh()
 
 def draw_last_page(screen, regex_to_color, file_iterator):
     file_iterator.seek_end()
     file_iterator.seek_to_one_page_before_end_of_file()
     redraw_screen(screen, regex_to_color, file_iterator)
-    safe_addstr_row_col(screen, file_iterator.term_num_rows, 0, 'Waiting for data... (interrupt to abort)')
+    safe_addstr_row_col(screen, file_iterator.term_dims.rows, 0, 'Waiting for data... (interrupt to abort)')
     screen.refresh()
 
-def tail_loop(screen, regex_to_color, file_iterator):
+def tail_loop(screen, regex_to_color, file_iterator, term_dims):
     while True:
         if screen.getch() == curses.KEY_RESIZE:
-            file_iterator.update_term_dimensions(screen)
+            term_dims.update(screen)
             file_iterator.seek_to_one_page_before_end_of_file()
             redraw_screen(screen, regex_to_color, file_iterator)
         draw_last_page(screen, regex_to_color, file_iterator)
         time.sleep(0.1)
 
-def enter_tail_mode(screen, regex_to_color, file_iterator):
+def enter_tail_mode(screen, regex_to_color, file_iterator, term_dims):
     screen.nodelay(1)
     curses.curs_set(0)
     try:
-        tail_loop(screen, regex_to_color, file_iterator)
+        tail_loop(screen, regex_to_color, file_iterator, term_dims)
     except KeyboardInterrupt:
         pass
     screen.nodelay(0)
@@ -184,15 +190,16 @@ def curses_init_colors():
 def main(screen, input_file, config_filepath):
     curses_init_colors()
     regex_to_color = load_config(config_filepath)
-    file_iterator = FileIterator(input_file, screen)
+    term_dims = TerminalDimensions(screen)
+    file_iterator = FileIterator(input_file, term_dims)
     redraw_screen(screen, regex_to_color, file_iterator)
     input_to_action = {ord(key): action for (key, action) in {
         'j' : lambda: file_iterator.forward_seek(1),
         'k' : lambda: file_iterator.reverse_seek(1),
-        'd' : lambda: file_iterator.forward_seek(file_iterator.term_num_rows / 2),
-        'u' : lambda: file_iterator.reverse_seek(file_iterator.term_num_rows / 2),
-        'f' : lambda: file_iterator.forward_seek(file_iterator.term_num_rows),
-        'b' : lambda: file_iterator.reverse_seek(file_iterator.term_num_rows),
+        'd' : lambda: file_iterator.forward_seek(term_dims.rows / 2),
+        'u' : lambda: file_iterator.reverse_seek(term_dims.rows / 2),
+        'f' : lambda: file_iterator.forward_seek(term_dims.rows),
+        'b' : lambda: file_iterator.reverse_seek(term_dims.rows),
         'g' : lambda: file_iterator.seek_start(),
         'G' : lambda: file_iterator.seek_to_one_page_before_end_of_file(),
         'q' : lambda: sys.exit(os.EX_OK)
@@ -204,10 +211,10 @@ def main(screen, input_file, config_filepath):
             input_to_action[user_input]()
         elif user_input == curses.KEY_RESIZE:
             screen.clear()
-            file_iterator.update_term_dimensions(screen)
+            term_dims.update(screen)
         elif user_input == ord('F'):
-            enter_tail_mode(screen, regex_to_color, file_iterator)
-            file_iterator.update_term_dimensions(screen)
+            enter_tail_mode(screen, regex_to_color, file_iterator, term_dims)
+            term_dims.update(screen)
             file_iterator.seek_to_one_page_before_end_of_file()
         redraw_screen(screen, regex_to_color, file_iterator)
 
