eff8ab9ee3419eeab6a86afec833850b77c598c9
John <johngkhs@gmail.com>
2016-12-15 19:50:50 -0600

    Modify config values to be more realistic

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ import collections
 import curses
 
 regex_to_color = collections.OrderedDict()
-regex_to_color['\d aaaaaa'] = 1
-regex_to_color['\d'] = 2
-regex_to_color['\d a'] = 4
-regex_to_color['c.*'] = 8
-regex_to_color['200'] = 254
+regex_to_color['[Ee]rror'] = curses.COLOR_RED
+regex_to_color['[Ii]nfo'] = curses.COLOR_BLUE
+regex_to_color['[Dd]ebug'] = curses.COLOR_CYAN
+regex_to_color['\d{3}'] = 150
